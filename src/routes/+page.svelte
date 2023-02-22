896b7bef1d77c6d49495cce13a17bc4715740fb1
Adam Andrews <andrewsadam07@gmail.com>
2023-02-22 02:37:32 +0000

    Added typewriter effect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,17 @@
 <script>
 	let typer = '';
+	const sentence = 'Hello my name is adam';
 </script>
 
-<div>{typer}</div>
-
+{#each sentence as letter, wordIndex}
+	<span class:selected={typer[wordIndex] === letter}> {letter} </span>
+{/each}
 <input placeholder="Hello" bind:value={typer} />
 
 <div>This is where the keyboard will be</div>
+
+<style>
+	.selected {
+		font-size: 5rem;
+	}
+</style>
